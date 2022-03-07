47f784321ff77275a1a075f59d99c1ae92886ce6
Willyams Yujra <yracnet@gmail.com>
2022-03-06 23:24:13 -0400

    + R2

--- js/template.js
+++ js/template.js
@@ -29,6 +29,114 @@ return /******/ (() => { // webpackBootstrap
 var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 
+//var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//  target: '#navbar-example',
+//});
+window.addEventListener(
+  'scroll',
+  function () {
+    const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    const otNavbar = document.querySelector('.ot-navbar');
+    console.log('scrollTop:', scrollTop);
+    if (scrollTop > 5) {
+      otNavbar.classList.add('ot-light');
+      otNavbar.classList.remove('ot-dark');
+    } else {
+      otNavbar.classList.remove('ot-light');
+      otNavbar.classList.add('ot-dark');
+    }
+  },
+  false
+);
+const nextIcon = '<i class="fa fa-long-arrow-right"/>';
+const prevIcon = '<i class="fa fa-long-arrow-left"/>';
+$(document).ready(function () {
+  $('.ot-owl-team .owl-carousel').owlCarousel({
+    items: 4,
+    nav: true,
+    loop: true,
+    margin: 31,
+    dots: false,
+    mouseDrag: true,
+    responsiveClass: true,
+    navText: [prevIcon, nextIcon],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 1,
+      },
+      767: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
+
+  $('.ot-owl-portfolio .owl-carousel').owlCarousel({
+    items: 3,
+    nav: true,
+    loop: true,
+    dots: false,
+    margin: 30,
+    mouseDrag: true,
+    responsiveClass: true,
+    navText: [prevIcon, nextIcon],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 1,
+      },
+      767: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+
+  $('.ot-owl-testimonial .owl-carousel').owlCarousel({
+    items: 1,
+    nav: true,
+    loop: true,
+    dots: false,
+    margin: 30,
+    mouseDrag: true,
+    responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    navText: [prevIcon, nextIcon],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 1,
+      },
+      767: {
+        items: 1,
+      },
+      992: {
+        items: 1,
+      },
+      1200: {
+        items: 1,
+      },
+    },
+  });
+});
 
 /******/ 	return __webpack_exports__;
 /******/ })()
